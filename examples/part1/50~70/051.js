14370448bb6cb849176249fa599c61e18c6b9b63
saeyoung <syoungbb25@gmail.com>
2022-01-22 14:08:27 +0900

    053. static키워드와 get 키워드를 통한class 정의

--- examples/part1/50~70/051.js
+++ examples/part1/50~70/051.js
@@ -1,4 +1,4 @@
-//클래스 정의하기 
+//1. 클래스 정의하기 
 // - ES6부터 class 키워드를 통해 클래스를 정의할 수 있다. 
 // - 별도 타입의 객체를 생성하는 설계 도면이라고 볼 수 있다. 
 
@@ -22,10 +22,43 @@ class Cart {          // class키워드를 통해 Cart를 정의
     }
 }
 
-const cart1 = new Cart();
+const cart1 = new Cart();            // const로 정의하여 다른 값을 대입할 수 없다. 
 
 cart1.addProduct({id:1, name:'노트북'})
 console.log(cart1.store);
 
 const p = cart1.getProduct(1);
 console.log(p);
+
+
+//2. 클래스 상속 이해하기 
+
+class Chart {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    drawLine() {
+        console.log('draw line');   // draw line
+    }
+}
+
+class BarChart extends Chart{          //클래스의 상속은 extends 키워드를 사용.
+        constructor(width, height){
+            super(width, height)      // 상속을 하게 되면 생성자 함수에서 상속한 부모 클래스의 생성자를 호출해야하므로 super 키워드를 사용. 
+        }
+
+        draw(){
+            this.drawLine();
+            console.log(`draw ${this.width} X ${this.height} barChart`);   // draw 100 X 100 barChart
+        }
+}
+
+const barChart1 = new BarChart(100, 100); 
+barChart1.draw();
+
+// * ES6의 클래스를 통한 상속은 생성자 함수의 프로토타입 기반의 상속과 크게 다르지 않다. 
+//   자바스크립트는 새로운 기능이 추가되면 하위 버전의 코드로 변환이 되어야한다 
+//   ES6의 클래스는 생성자 함수로 변환, extends 키워드의 상속은 prototype 기반 상속 코드로 변경될 수 있다. 
+//   자바스크립트는 여전히 프로토타입 기반의 상속이다. 
---
+++ examples/part1/50~70/053.js
@@ -0,0 +1,53 @@
+//1. 클래스 정적 메소드와 속성 정의하기 
+
+// 일반적인 메소드는 해당 클래스이 인스턴스를 통해 호출된다. 
+// 정적 메소드는 클래스를 통해 직접 호출하는 메소드를 말한다. 
+// 클래스에서 정적 메소드는 static 키워드를 사용하여 정의한다. 
+
+class Product {
+    static build(name, price) {                               // static 키워드를 사용하여 bulid 정적 메소드를 정의 
+        const id = Math.floor(Math.random()* 1000 );     // Math.random 함수를 호출하면  0부터 1까지의 난수가 반환된다. 
+        return new Product(id, name, price);            // bulid 정적 메소드는 난수를 아이드로 하는 상품 인스턴스를 반환한다. 
+    }
+
+    static getTaxPrice(product){                  // static 키워드를 사용하여 getTaxPrice  정적 메소드를 정의.
+        return (product.price * 0.1) + product.price;
+    }
+
+    constructor(id, name, price){        // 상품 클래스의 생성자 함수를 정의 
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+}
+
+class DeposableProduct extends Product {    // 폐기가 가능한 클래스를 정의, DeposableProduct클래스는 product 클래스를 상속한다. 
+    depose(){
+        this.deposed = true;
+    }
+}                                      // * 생성자 함수의 prototype 기반 상속과는 다르게 클래스로 상속을 하게 되면 정적 메소드 또한 상속하게 된다. 
+
+const gum = Product.build('껌', 1000);
+console.log(gum);   // 랜덤하게 부여된 아이디 ..   id:200, name: '껌', price: 1000
+
+const clothes = new DeposableProduct(1, '옷', 2000);   // DeposableProduct 인스턴스를 생성 
+const taxPrice = DeposableProduct.getTaxPrice(clothes);
+console.log(taxPrice); // DeposableProduct 클래스에서 getTaxPrice 정적 메소드를 정의하지 않았지만 Product 클래스를 상속하였기 때문에 호출이 가능.! 
+                       // 2200
+
+// * 클래스를 정의할 때 정적 속성 또한 static 키워드와 get 키워드를 통해 정의할 수 있다. 
+
+class ProductWithCode {
+    static get CODE_PREFIX(){
+       return "PRODUCT-"
+    }
+
+    constructor(id) {
+        this.id
+        this.code = ProductWithCode.CODE_PREFIX + id;
+    }
+}
+
+const product1 = new ProductWithCode('001');
+console.log(ProductWithCode.CODE_PREFIX);
+console.log(product1.code);
