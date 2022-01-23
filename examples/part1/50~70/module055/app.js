f136abc93fd8732a91401ef676ebc916b2e1cc04
saeyoung <syoungbb25@gmail.com>
2022-01-23 12:29:30 +0900

    part3 시작/ 060. 표준 내장 객체 이해하기

--- examples/part1/50~70/module055/app.js
+++ examples/part1/50~70/module055/app.js
@@ -1,59 +1,59 @@
-import { version as moduleVersion } from './version.js';   // as키워드를 사용하면 현재 모듈에서 다른 이름으로 사용할 수 있다. 
+// import { version as moduleVersion } from './version.js';   // as키워드를 사용하면 현재 모듈에서 다른 이름으로 사용할 수 있다. 
 
-const version = 'v0';
-console.log(moduleVersion); // v1.0
+// const version = 'v0';
+// console.log(moduleVersion); // v1.0
 
-// - vesion 이름으로 상수를 이미 선언했기 때문에 version.js 에서 내보낸 이름으로 
-//   version을 가져올 수 없다. 그래서 moduleVersion으로 바꿔 가져온다 
+// // - vesion 이름으로 상수를 이미 선언했기 때문에 version.js 에서 내보낸 이름으로 
+// //   version을 가져올 수 없다. 그래서 moduleVersion으로 바꿔 가져온다 
 
-// ** as 키워드
-//   -  export 할 때에도 사용할 수 있다. 
-//ex
-const version = 'v1.0';
-export {version as ver};
+// // ** as 키워드
+// //   -  export 할 때에도 사용할 수 있다. 
+// //ex
+// const version = 'v1.0';
+// export {version as ver};
 
-// * (별표)를 이용하거나 다른 모듈의 코드를 실행만 시킬 수도 있다. 
+// // * (별표)를 이용하거나 다른 모듈의 코드를 실행만 시킬 수도 있다. 
 
-// add.js
-export default function add(a,b){
-    return a+b;
-}
-export const version = 'v1.0';
-// add함수를 모듈의 기본으로 정의, verson 변수를 내보낸다. 
+// // add.js
+// export default function add(a,b){
+//     return a+b;
+// }
+// export const version = 'v1.0';
+// // add함수를 모듈의 기본으로 정의, verson 변수를 내보낸다. 
 
-//sideeffect.js
-console.log('hello!');   
-window.hello = function hello(name) {
-    console.log('hello ' + name);
-}
-//외부로 내보내는 값 없이 콘솔에 출력하거나
-//전역 객체인 window에 메소드로 hello를 선언
-// window에 메소드를 추가하면 window를 통하지 않고 직접 해당 메소드의 호출이 가능
-
-
-//app2.js
-import * as add from './add.js';  // add.js 모듈을 *을 이용하여 전체를 가져온다. / 모듈 전체를 가리키는 이름이 있어야 하므로 as를 사용하여 이름을 줌.(add)
-import './sideeffect.js';  // from 작성안함. 
-
-console.log(add.verson);  
-const added = add.default(1,2); // import로 sideeffect.js 모듈을 실행했기 때문에 전역으로 선언된 hello함수를 실행 할 수 있음. 
-console.log(added);  
-
-hello('harin');
-
-// => 다른 모듈에서 가져온 값들은 복제되는 것이 아니라 이름과 연결된 그 자체를 가져오게 된다.
-// => 내보낸 모듈에세 값을 변경하게 되면 가져온 모듈에서도 영향을 받게 된다. 
+// //sideeffect.js
+// console.log('hello!');   
+// window.hello = function hello(name) {
+//     console.log('hello ' + name);
+// }
+// //외부로 내보내는 값 없이 콘솔에 출력하거나
+// //전역 객체인 window에 메소드로 hello를 선언
+// // window에 메소드를 추가하면 window를 통하지 않고 직접 해당 메소드의 호출이 가능
 
 
-// value.js
-export let value = 1;    // value 변수에 1을 할당
+// //app2.js
+// import * as add from './add.js.js';  // add.js 모듈을 *을 이용하여 전체를 가져온다. / 모듈 전체를 가리키는 이름이 있어야 하므로 as를 사용하여 이름을 줌.(add)
+// import './sideeffect.js.js';  // from 작성안함. 
 
-setTimeout(()=> {
-    value++;
-}, 1000);
+// console.log(add.verson);  
+// const added = add.default(1,2); // import로 sideeffect.js 모듈을 실행했기 때문에 전역으로 선언된 hello함수를 실행 할 수 있음. 
+// console.log(added);  
 
-//app3.js
-import {value} from './value.js';   // value.js 모듈로부터 value를 가져와 
-console.log(value);                 // 콘솔에 출력 // 1
+// hello('harin');
+
+// // => 다른 모듈에서 가져온 값들은 복제되는 것이 아니라 이름과 연결된 그 자체를 가져오게 된다.
+// // => 내보낸 모듈에세 값을 변경하게 되면 가져온 모듈에서도 영향을 받게 된다. 
+
+
+// // value.js
+// export let value = 1;    // value 변수에 1을 할당
+
+// setTimeout(()=> {
+//     value++;
+// }, 1000);
+
+// //app3.js
+// import {value} from './value.js.js';   // value.js 모듈로부터 value를 가져와 
+// console.log(value);                 // 콘솔에 출력 // 1
   
-setTimeout(()=> console.log(value), 2000);   // 2
+// setTimeout(()=> console.log(value), 2000);   // 2
---
+++ examples/part1/part3/060.js
@@ -0,0 +1,67 @@
+// 1. 표준 내장 객체 이해하기
+// - 자바스크립트의 표준 내장 객체에대 
+
+// ECMAScript 구현 명세에 의해 정의된 표준 내장 객체(Standard Bulit-in Objects 또는 Global Objects)
+// (1) 표준 내장 객체
+//     전역 스코프(scope)안에 있는 객체들을 참조하고 있어서, 자바스크립트를 실행할 
+//     어떤 환경(브라우져, 서버)이라도 전역에서 사용 가능.
+//  - 내장 함수 객체(Built-in Function Object)
+//    객체임임에도 불구하고 함수처럼 호출할 수 있음. 
+//    매개변수를 받고, 함수 몸통에 내장 함수 객체의 행위를 특징화 하는 코드들이 구현.
+//  - new 지시자를 사용하여 함수 형태로 호출.
+//  - Constructor(생성자)를 생성.
+//  - 표준 내장 객체 내부에는 속성과 메소드가 정의되어 있음.
+
+const str = new String('자바스크립트');   // new를 이용하여 생성자를 만들고 변수에 선언
+const num = new Number(200);
+const bool = new Boolean(true);
+const date = new Date();
+const map = new Map();
+const set = new Set();
+
+console.log(Math.PI);                    //선언없이 객체의 메소드와 속성을 바로 가져다 사용할 수 있음. 
+console.log(Date.parse('2019-01-01'));
+console.log(JSON.parse('{}'));
+
+
+//1-1. 내장 객체와 원시 자료형의 차이점
+
+const str1 = '자바스크립트 200제';
+const str2 = new String('자바스크립트 200제');
+
+console.log(typeof str1);  //string
+console.log(typeof str2);  //object
+
+console.log(str1 === '자바스크립트 200제'); // true
+console.log(str2 === new String('자바스크립트 200제')); // false
+// String객체로 대입된 str2는 값이 아닌 주소값을 참조. 
+
+console.log(str1.valueOf()); //자바스크립트 200제
+console.log(str2.valueOf()); //자바스크립트 200제 
+
+// (1) 문자열로 작성된 값이 String 객체로 자동으로 래핑된다.
+// (2) typeof : 변수에 할당된 값에 대한 자료형 정보를 문자열로 반환. 
+// (3) valueof() : String 내장 객체에 정의된 메소드. 
+//                 객체의 원시형 값, 즉 문자열을 반환하는 메소드 
+
+
+//1-2. 자바스크립트 표준 내장 객체
+// (1) Object
+// (2) Number
+// (3) String
+// (4) Array : 모든 배열은 Array.prototype을 상속받음. 
+// (5) Math : 다른 내장 객체와 달리 Math 객체는 new를 통해 인스턴스를 생성하지 않음.
+//            static으로 정의된 속성과 메소드를 직접 호출해야함. 
+// (6) Date
+// (7) JSON
+// (8) RegExp : 정규표현식. 특정 문자열 처리를 위해 사용하는 문자열 패턴 정의를 의미.
+//              정규표현식을 적용 가능한 문자열 탐색, 비교, 대체 등의 문자열 처리 메소들을 지원.
+// (9) Map : ES6부터 표준으로 추가.
+//           Map객체는 키 :값 데이터 구조를 지닌 데이터 집합체(Collection)
+//           키의 중복성을 허용하지 않으며 관련된 속성, 메소드들을 갖고 있음.
+//           Iterator를 통해 Map의 데이터를 순회하는 특징이 있음. 
+// (10) Set : ES6부터 표준으로 추가된 객체형
+//            오직 값으로 이루어진 데이터 집합체
+//            배열과 비슷하게 느껴질 수도 있으나 내부 속성, 메소드의 구성이 다르고, 
+//            Iterator로 순회된다는 점과 값의 중복성을 허용하지 않는 차이점이 있음. 
+
