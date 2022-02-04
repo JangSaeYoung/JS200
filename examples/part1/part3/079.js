14903c547217ae51fec9250358f1d9071f2d0c49
saeyoung <syoungbb25@gmail.com>
2022-02-04 15:09:09 +0900

    088. slice

--- examples/part1/part3/079.js
+++ examples/part1/part3/079.js
@@ -38,3 +38,142 @@ capitals.split('\n').forEach( s => {
 // Paris is in France
 // Madrid is in Spain
 // Rome is in Italy 
+
+
+
+// * 배열 뒤에 요소 추가하기 - push
+// - 자바스크립트 배열 자료형은 Linked List 자료구조 형태 => 배열 앞과 뒤에 요소 추가 가능
+// - Array 내장객체 메소드
+// - 배열 뒤에서 요소를 추가
+
+const festa = ['mang'];
+festa.push('chimmy');
+festa.push('tata');
+festa.push('cooky');
+festa.push('shooky');
+festa.push('koya');
+festa.push('rj');
+
+festa.forEach(name => {
+    console.log(name);
+});
+
+console.log(festa);
+
+// * 배열 앞에 요소 추가하기 - unshfit
+// - Array 내장 객체 메소드
+// - 배열 맨 앞에 요소를 추가하는 함수. 
+
+ const festa1 = ['mang'];
+festa1.unshift('chimmy');
+festa1.unshift('tata');
+festa1.unshift('cooky');
+festa1.unshift('shooky');
+festa1.unshift('koya');
+festa1.unshift('rj');
+
+festa1.forEach(name => {
+    console.log(name);
+});
+
+console.log(festa1);
+
+
+
+// * 배열 길이 구하기 - length
+// - Array 내장객체 속성
+
+const ship = {
+    max: 4,
+    passengers: [],
+    onBoard: function(name){
+        if( this.passengers.length === 4){
+            console.log(`This ship is full. ${name} can not board this ship.`);
+        }else{
+            this.passengers.push(name);
+            console.log(`${name} boarded.`);
+        }
+    }
+} 
+
+ship.onBoard('chloe');
+ship.onBoard('jay');
+ship.onBoard('david');
+ship.onBoard('asher');
+ship.onBoard('daniel');   // This ship is full. daniel can not board this ship.
+console.log.apply(ship.passengers);
+
+
+
+
+// * 배열 함치기 - concat 
+// - 여러 개의 배열을 하나로 합치는 방법
+// - 여러 인자를 넣는 것이 가능하고 하나의 배열로 반환하며,
+//   배열 내부에는 인자에 넣은 순서대로 요소가 나열된다. 
+
+const prevList = [1,2,3];
+const currentList = [4,5,6];
+const nextList = [7,8,9];
+
+console.log(prevList.concat(currentList)); // [1,2,3,4,5,6]
+console.log(prevList.concat(nextList)); // [1,2,3,7,8,9]
+console.log(prevList.concat(currentList, nextList));// [1,2,3,4,5,6,7,8,9]
+
+console.log(['배열'].concat('합치기'));
+console.log(['배열'].concat('합치기', ' JavaScript200'));
+
+
+
+// * 배열에 특정 구분자 넣어 문자형으로 변환하기 - join
+// - Arrar 객체 메소드 
+// - 각 배열 요소를 병합하여 하나의 문자열로 변환.
+
+const dialogue = [
+    'Fear is the path to the dark side',
+    'Fear leads to anger',
+    'Anger leads to hate',
+    'Hate leads to suffering',
+    'I sense much fear in you.'
+];
+
+console.log(dialogue.join('.'));
+console.log(dialogue.join('. \n'));
+
+// * 배열 마지막 요소 추출하기 - pop
+// - 배열에서 마지막에 있는 요소만 추출하는 방법
+// - 원본 배열도 함께  수정. 
+
+const arr = [1,2,3];
+console.log(arr.pop()); // 3
+console.log(arr.pop()); // 2
+
+console.log(arr); // [1]
+
+console.log(arr.pop()); // 1
+console.log(arr.pop()); // undefined
+
+
+
+// * 배열 맨 앞 요소 추출하기 - shift
+// - 배열에서 맨 앞 요소를 추출하는 방법
+// - pop과 동일하게 원본 배열도 함께 수정이 됨. 
+
+const arr1 = [1,2,3];
+console.log(arr1.shift()); // 1
+console.log(arr1.shift()); // 2
+console.log(arr1.shift()); // 3
+console.log(arr1.shift()); // undefined
+
+
+// * 배열 특정 위치의 요소 추출하기 - slice
+// - Array 메소드
+// - 인덱스의 시작과 끝을 지정하여 배열 요소를 추출
+// - 인덱스에 있는 요소는 제외하고 그 이전까지의 요소들을 추출.
+// 배열.slice(시작 인덱스, 끝 인덱스)
+
+// - slice 함수는 원본 배열을 변경하지 않고 복제 (얕은 복사)를 수행
+// - slice 함수를 통해 처리된 결과값을 활용하려면 별도의 변수로 대입해야 한다. 
+
+const arr2 = ['melon', 'lemon', 'source', 'apple', 'juice'];
+console.log(`과일이 아닌 요소는 ${arr2.slice(2,3)}와 ${arr2.slice(4,5)} 입니다.`); // 과일이 아닌 요소는 source와 juice 입니다.
+console.log(arr2.slice(0,10)); // ['melon', 'lemon', 'source', 'apple', 'juice']
